a2498acf20692980afbf75a3e57f9d101fedd94a
bujniasz <bujnowski.aleksander@gmail.com>
2025-05-31 20:11:46 +0200

    add error message test

--- tests/test_kmer_count.py
+++ tests/test_kmer_count.py
@@ -2,6 +2,7 @@ import json
 import polars as pl
 import polars_bio as pb
 from polars_bio.io import read_fastq
+import pytest
 
 def run_kmer_test(k: int, json_path: str):
     result = pb.sql(f"SELECT kmer_count(sequence, {k}) AS result FROM example").collect()
@@ -22,3 +23,11 @@ def test_kmer_count_matches_expected():
 
     run_kmer_test(3, "fastqc-rs_output/fastqc_kmer3.json")
     run_kmer_test(5, "fastqc-rs_output/fastqc_kmer5.json")
+
+def test_kmer_count_invalid_k_raises():
+    read_fastq("example.fastq")  # rejestruje tabelę 'example'
+
+    with pytest.raises(Exception) as exc_info:
+        pb.sql("SELECT kmer_count(sequence, -2) AS result FROM example").collect()
+
+    assert "k must be greater than 0" in str(exc_info.value)
