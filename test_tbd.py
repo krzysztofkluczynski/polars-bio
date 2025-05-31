96f32c117fe4fc963433abb689f26dda34172a81
bujniasz <bujnowski.aleksander@gmail.com>
2025-05-31 18:16:19 +0200

    add fastqc-rs output

--- test_tbd.py
+++ test_tbd.py
@@ -3,14 +3,12 @@ import pandas as pd
 from polars_bio.io import read_fastq
 import polars as pl
 
-
-
-df = read_fastq("example2.fastq")
+df = read_fastq("example.fastq")
 
 print(df)
 print(df.schema)
 pl.Config.set_tbl_rows(100)
-print(pb.sql("SELECT kmer_count(sequence, 5) FROM example2 LIMIT 60").collect())
+print(pb.sql("SELECT kmer_count(sequence, 1) AS result FROM example LIMIT 60").collect())
 
 
 #counts = pb.count_kmers(df, k=5, threads=8)
